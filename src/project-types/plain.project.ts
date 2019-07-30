04938770ee3fdbb06b67e043f5b4bdee997e2d44
Alex Tykhonenko <alexandr.tykhonenko@requestum.com>
2019-07-30 11:28:19 +0300

    fix installation

--- src/project-types/plain.project.ts
+++ src/project-types/plain.project.ts
@@ -8,7 +8,7 @@ export const plainProject = ({ title }: Answer = { title: '' } as Answer): Listr
   return new Listr([
     {
       title: 'Install Plain JS project',
-      task: () => command(`git clone https://github.com/Alevettih/unifier-cli.git ${join(title)}`)
+      task: () => command(`git clone https://github.com/Alevettih/project-template-gulp.git ${join(title)}`)
     },
     {
       title: 'Specify it...',
