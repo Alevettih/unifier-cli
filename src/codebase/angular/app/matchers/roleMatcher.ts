7b570f8fe8fa99d61692227af18f3311ceace3e4
Alex tykhonenko <alevettih@gmail.com>
2020-03-18 09:56:17 +0200

    Feature: Error message in roleMatcher.ts

--- src/codebase/angular/app/matchers/roleMatcher.ts
+++ src/codebase/angular/app/matchers/roleMatcher.ts
@@ -4,9 +4,12 @@ import { STORAGE_KEYS } from '@misc/constants';
 export function roleMatcher(segments: UrlSegment[], group: UrlSegmentGroup, { data, path }: Route): UrlMatchResult {
   const storedRole = sessionStorage.getItem(STORAGE_KEYS.ROLE) || localStorage.getItem(STORAGE_KEYS.ROLE);
   if (data.roles.includes(storedRole) && !path) {
-    console.log(true);
     return { consumed: [] };
   }
+
+  console.error(
+    `Stored role hasn't match with any role in array. Stored role is "${storedRole}", but role(s) to pass is "${data.roles}"`
+  );
 
   return null;
 }
