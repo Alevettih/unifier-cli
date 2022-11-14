5ecfa65f4d35480c5031229f574c5c0b63811399
Alex Tikhonenko <alevettih@gmail.com>
2022-11-14 16:16:42 +0300

    Feat: update angular codebase

--- src/codebase/angular/src/app/interceptors/mock/responses/token.responses.ts
+++ src/codebase/angular/src/app/interceptors/mock/responses/token.responses.ts
@@ -3,6 +3,7 @@ import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { IApiTokens } from '@models/interfaces/api-tokens.interface';
 import { ILoginParams } from '@models/interfaces/login-params.interface';
 import { UserRole } from '@models/enums/user-role.enum';
+import { GrantType } from '@models/enums/grant-type.enum';
 
 export interface ITokensResponses {
   accessToken(routeParams: string[], body: ILoginParams): Observable<HttpResponse<IApiTokens>>;
@@ -11,7 +12,8 @@ export interface ITokensResponses {
 export const tokensResponses: ITokensResponses = {
   accessToken(routeParams: string[], body: ILoginParams): Observable<HttpResponse<IApiTokens>> {
     const username: string = (body instanceof FormData ? ((body as FormData).getAll('username')[0] as string) : body?.username) ?? '';
-    const refresh: string = (body instanceof FormData ? ((body as FormData).getAll('refresh_token')[0] as string) : body?.username) ?? null;
+    const refresh: string = (body instanceof FormData ? ((body as FormData).getAll('refresh_token')[0] as string) : body?.token) ?? null;
+    const grantType: string = (body instanceof FormData ? ((body as FormData).getAll('grant_type')[0] as string) : body?.grantType) ?? null;
     const tokenRole: string = username?.split?.('@')?.[0];
     const token: string = refresh ?? btoa(Object.values(UserRole).includes(tokenRole as UserRole) ? tokenRole : '');
     const entity: IApiTokens = {
@@ -19,7 +21,7 @@ export const tokensResponses: ITokensResponses = {
       refresh_token: token
     };
 
-    if (token) {
+    if (grantType === GrantType.clientCredentials || token) {
       return of(new HttpResponse({ status: 200, body: entity }));
     } else {
       throw new HttpErrorResponse({ status: 401, error: { message: 'Access Denied' } });
