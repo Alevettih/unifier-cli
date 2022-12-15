e83a1af7ff5ad264443b7eff1fe1522ddfb229e3
Alex Tikhonenko <alevettih@gmail.com>
2022-12-15 11:43:40 +0300

    Feat: remove jest

--- src/misc/helpers/command-with-output.helper.ts
+++ src/misc/helpers/command-with-output.helper.ts
@@ -1,10 +1,9 @@
-import { filter, from, map, merge, Observable, startWith } from 'rxjs';
-import { ExecaChildProcess, ExecaReturnValue } from 'execa';
-import { observableFromStream } from '@helpers/observable-from-stream.helper';
 import split from 'split';
-import { command, Options } from 'execa';
-import { OutputFormatter } from '@helpers/output-formatter.helper';
 import { Readable, Writable } from 'stream';
+import { filter, from, map, merge, Observable, startWith } from 'rxjs';
+import { command, Options, ExecaChildProcess, ExecaReturnValue } from 'execa';
+import { OutputFormatter } from '@helpers/output-formatter.helper';
+import { observableFromStream } from '@helpers/observable-from-stream.helper';
 
 export function commandWithOutput(cm: string, options?: Options): Observable<string> {
   const childProcess: ExecaChildProcess = command(cm, options);
